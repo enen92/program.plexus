977c6c5a2df81f9f109f5ae2e9e18af5b73037b4
enen92 <92enen@gmail.com>
2014-09-02 14:52:16 +0100

    fucking indentation

--- plugin.video.p2p-streams/default.py
+++ plugin.video.p2p-streams/default.py
@@ -97,9 +97,9 @@ if mode==None or url==None or len(url)<1:
       print("Installed version: v" + versao)
       if settings.getSetting('autoconfig') == "true": first_conf()
       else:
-          try:
-      	    if settings.getSetting('last_version_check') != versao: check_for_updates()
-      	except: pass
+          if settings.getSetting('last_version_check') != versao:
+              try:check_for_updates()
+              except: pass
       main_menu()
 elif mode==1: ace.acestreams(name,iconimage,url)
 elif mode==2: sop.sopstreams(name,iconimage,url)
