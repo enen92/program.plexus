7bda0a2d2a1e58732af77d60ede89be4c57228c7
enen92 <92enen@gmail.com>
2015-08-14 23:55:44 +0100

    translation wip

--- resources/plexus/plexusutils/utilities.py
+++ resources/plexus/plexusutils/utilities.py
@@ -7,7 +7,6 @@
     Functions:
     
     handle_wait(time_to_wait,title,text,segunda='') -> Timer with dialog progress capabilities
-    clean_text(text) -> Function to remove specific characters from a string
     getDirectorySize(directory) -> returns a directory size recursively
     recursive_overwrite(src, dest, ignore=None) -> Copy and replace an entire directory recursively
    	
@@ -26,7 +25,7 @@ def handle_wait(time_to_wait,title,text,segunda=''):
                 secs = secs + 1
                 percent = increment*secs
                 secs_left = str((time_to_wait - secs))
-                if segunda=='': remaining_display = translate(40188) + str(secs_left) + translate(40189)
+                if segunda=='': remaining_display = translate(30070) + str(secs_left) + translate(30071)
                 else: remaining_display=segunda
                 mensagemprogresso.update(percent,text,remaining_display)
                 xbmc.sleep(1000)
@@ -39,11 +38,6 @@ def handle_wait(time_to_wait,title,text,segunda=''):
                 mensagemprogresso.close()
                 return False
 
-def clean_text(text):
-      command={'\r':'','\n':'','\t':'','&nbsp;':' ','&quot;':'"','&#039;':'','&#39;':"'",'&#227;':'ã','&170;':'ª','&#233;':'é','&#231;':'ç','&#243;':'ó','&#226;':'â','&ntilde;':'ñ','&#225;':'á','&#237;':'í','&#245;':'õ','&#201;':'É','&#250;':'ú','&amp;':'&','&#193;':'Á','&#195;':'Ã','&#202;':'Ê','&#199;':'Ç','&#211;':'Ó','&#213;':'Õ','&#212;':'Ó','&#218;':'Ú'}
-      regex = re.compile("|".join(map(re.escape, command.keys())))
-      return regex.sub(lambda mo: command[mo.group(0)], text)
-      
 def getDirectorySize(directory):
 	dir_size = 0
 	for (path, dirs, files) in os.walk(directory):
